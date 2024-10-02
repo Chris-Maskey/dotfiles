bc54dcb5c5a34638714539e66ad4abd998151783
Chris-Maskey <chrismaskey03@gmail.com>
2024-10-02 11:33:13 +0000

    add codeium

--- nvim/lua/josean/core/keymaps.lua
+++ nvim/lua/josean/core/keymaps.lua
@@ -22,5 +22,5 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
---Dismiss Noice Message
-keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
+-- --Dismiss Noice Message
+-- keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
