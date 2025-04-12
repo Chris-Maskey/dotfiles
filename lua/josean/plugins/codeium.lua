509991d4eaad7b2bb8f22e33ddbd67ae588b752b
Chris-Maskey <chrismaskey03@gmail.com>
2025-04-12 17:20:30 +0545

    Add neocodeium

--- lua/josean/plugins/codeium.lua
+++ lua/josean/plugins/codeium.lua
@@ -1,21 +1,31 @@
+-- return {
+-- "Exafunction/codeium.vim",
+-- event = "BufEnter",
+-- config = function()
+--   vim.keymap.set("i", "<Tab>", function()
+--     return vim.fn["codeium#Accept"]()
+--   end, { expr = true, silent = true })
+--   vim.keymap.set("i", "<c-;>", function()
+--     return vim.fn["codeium#CycleCompletions"](1)
+--   end, { expr = true, silent = true })
+--   vim.keymap.set("i", "<c-,>", function()
+--     return vim.fn["codeium#CycleCompletions"](-1)
+--   end, { expr = true, silent = true })
+--   vim.keymap.set("i", "<c-x>", function()
+--     return vim.fn["codeium#Clear"]()
+--   end, { expr = true, silent = true })
+--   vim.g.codeium_filetypes = {
+--     markdown = false,
+--   }
+-- end,
+-- }
+
 return {
-  -- "Exafunction/codeium.vim",
-  -- event = "BufEnter",
-  -- config = function()
-  --   vim.keymap.set("i", "<Tab>", function()
-  --     return vim.fn["codeium#Accept"]()
-  --   end, { expr = true, silent = true })
-  --   vim.keymap.set("i", "<c-;>", function()
-  --     return vim.fn["codeium#CycleCompletions"](1)
-  --   end, { expr = true, silent = true })
-  --   vim.keymap.set("i", "<c-,>", function()
-  --     return vim.fn["codeium#CycleCompletions"](-1)
-  --   end, { expr = true, silent = true })
-  --   vim.keymap.set("i", "<c-x>", function()
-  --     return vim.fn["codeium#Clear"]()
-  --   end, { expr = true, silent = true })
-  --   vim.g.codeium_filetypes = {
-  --     markdown = false,
-  --   }
-  -- end,
+  "monkoose/neocodeium",
+  event = "VeryLazy",
+  config = function()
+    local neocodeium = require("neocodeium")
+    neocodeium.setup()
+    vim.keymap.set("i", "<Tab>", neocodeium.accept)
+  end,
 }
